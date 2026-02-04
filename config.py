1ac2a25816ae3ce60d968f2be9060420ab52418e
deidexdd-hash <deidex.dd@gmail.com>
2026-02-04 12:06:04 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,25 @@
+# config.py
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class Config:
-    BOT_TOKEN = os.getenv('BOT_TOKEN')
-    GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+    # Токен бота Telegram (обязательный)
+    BOT_TOKEN = os.getenv('BOT_TOKEN', '')
+    
+    # Groq API (опциональный)
+    GROQ_API_KEY = os.getenv('GROQ_API_KEY', '')
     GROQ_MODEL = os.getenv('GROQ_MODEL', 'mixtral-8x7b-32768')
+    
+    # Настройки для Render
+    PORT = int(os.environ.get('PORT', 8080))
     
     # Парсинг гороскопов
     HOROSCOPE_SOURCES = [
         "https://horo.mail.ru/prediction/",
         "https://www.astromeridian.ru/horoscope/",
-        "https://horo.rambler.ru/"
+        "https://hooro.rambler.ru/"
     ]
     
     # Кэширование
