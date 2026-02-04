932038a940e9db4235bc808fc529a2d1ef2aa8df
deidexdd-hash <deidex.dd@gmail.com>
2026-02-04 13:41:19 +0400

    Update config.py

--- config.py
+++ config.py
@@ -3,18 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Config:
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-    GROQ_MODEL = os.getenv("GROQ_MODEL", "mixtral-8x7b-32768")
-
-    # Параметры парсинга гороскопов
-    HOROSCOPE_SOURCES = [
-        "https://horo.mail.ru/prediction/",
-        "https://www.astromeridian.ru/horoscope/",
-        "https://horo.rambler.ru/",
-    ]
-
-    # Кэширование
-    CACHE_TTL = 3600  # 1 час
+    BOT_TOKEN      = os.getenv("BOT_TOKEN")
+    GROQ_API_KEY   = os.getenv("GROQ_API_KEY")
+    GROQ_MODEL     = os.getenv("GROQ_MODEL", "mixtral-8x7b-32768")
